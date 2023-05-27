105b769ac11fcb2e834e08bd38ac0164acb479ff
Sergey Maximov <maksimov569@gmail.com>
2023-05-27 15:48:30 +0500

    First interation of api_yamdb.

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -5,36 +5,38 @@ class Moder(permissions.BasePermission):
     """
     Return true for moders.
     """
+
     def has_permission(self, request, view):
-        return request.user.is_authenticated and (
-               request.user.is_admin
-               or request.user.is_superuser
-               or request.user.is_moder)
+        return request.user.is_authenticated and (request.user.is_admin
+                                                  or request.user.is_superuser
+                                                  or request.user.is_moder)
 
     def has_object_permission(self, request, view, obj):
         return request.user.is_authenticated and (
-               request.user.is_admin
-               or request.user.is_superuser
-               or request.user.is_moder)
+            request.user.is_admin
+            or request.user.is_superuser
+            or request.user.is_moder)
 
 
 class Admin(permissions.BasePermission):
     """
     Return True for admins.
     """
+
     def has_permission(self, request, view):
         return request.user.is_authenticated and (
-                request.user.is_admin or request.user.is_superuser)
+            request.user.is_admin or request.user.is_superuser)
 
     def has_object_permission(self, request, view, obj):
         return request.user.is_authenticated and (
-                request.user.is_admin or request.user.is_superuser)
+            request.user.is_admin or request.user.is_superuser)
 
 
 class Owner(permissions.BasePermission):
     """
     Let owner change its own content.
     """
+
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
@@ -52,5 +54,5 @@ class Owner(permissions.BasePermission):
 class ReadOnlyOrAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.method in permissions.SAFE_METHODS or (
-                request.user.is_authenticated
-                and (request.user.is_admin or request.user.is_superuser))
+            request.user.is_authenticated
+            and (request.user.is_admin or request.user.is_superuser))
